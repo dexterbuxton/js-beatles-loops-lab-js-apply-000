52a4467a637441b63af62b55ecc38c18e706f2ad
Dexter Buxton <dexter.buxton@gmail.com>
2018-11-26 00:15:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,17 +2,14 @@
 function theBeatlesPlay(musicians, instruments){
   var concert = [];
   var length = musicians.length
-  for (i=0; i < length, i++){
+  
+  // Loop through each musician
+  for (i=0; i < length; i++){
     let musician = musicians[i]
     let instrument = instruments[i]
     let action = musician + " plays " + instrument
     concert.push(action)
   }
+  return concert
 }
 
-
-var len = cars.length;
-var text = "";
-for (; i < len; i++) { 
-    text += cars[i] + "<br>";
-}
