c679191b5975f8db2b1f09e3f7dd4097914470e6
Dexter Buxton <dexter.buxton@gmail.com>
2018-11-26 00:19:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments){
   var concert = [];
-  var length = musicians.length
+  var length = musicians.length;
   
   // Loop through each musician
   for (i=0; i < length; i++){
-    let musician = musicians[i]
-    let instrument = instruments[i]
-    let action = musician + " plays " + instrument
-    concert.push(action)
+    let musician = musicians[i];
+    let instrument = instruments[i];
+    let action = musician + " plays " + instrument;
+    concert.push(action);
   }
-  return concert
+  return concert;
 }
 
+function johnLennonFacts(facts){
+  var length = facts.length;
+  var excitingFacts = [];
+  for (i=0; i < length; i++) {
+    let newFact = facts[0] + "!!!";
+    excitingFacts.push(newFact);
+  }
+  return excitingFacts;
+}
+
+function iLoveTheBeatles(number){
+  
+}
